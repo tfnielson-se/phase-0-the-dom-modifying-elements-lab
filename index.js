ba90dce3ba81dfe1c3b533dae29c1e6bf6f4451a
Thomas <tfnielson.se@gmail.com>
2022-12-13 16:39:55 -0700

    commit tree 1b47f6107c06d0e287d8c6eacc02c59689548541

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector(`main#main`).remove(`#main`)
+
+const newHeader = document.createElement(`h1`)
+newHeader.setAttribute(`id`, `victory`)
+newHeader.textContent = `Thomas is the champion`
+
